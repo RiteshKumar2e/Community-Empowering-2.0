bdffd8b693669e49d54b6646698d21cc4a951e49
RiteshKumar2e <riteshkumar90359@gmail.com>
2026-01-26 22:22:44 +0530

    Frontend updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -157,7 +157,7 @@ const Login = () => {
                                 theme="outline"
                                 size="large"
                                 text="signin_with"
-                                width="400"
+                                width="340"
                                 logo_alignment="center"
                                 useOneTap={false}
                                 auto_select={false}
